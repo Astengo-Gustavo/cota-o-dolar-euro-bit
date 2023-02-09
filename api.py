2af29ce10014bfe4b388472bf9500b332c083512
Astengo <astengo@pop-os.localdomain>
2023-02-08 23:19:45 -0300

    Api de cotação

---
+++ api.py
@@ -0,0 +1,14 @@
+import requests, json
+import time
+import datetime
+
+while True:
+   r = requests.get('https://api.hgbrasil.com/finance/quotations?key=127afa60')
+   cotacao = json.loads(r.text)
+
+   print('#### cotação ####     ', datetime.datetime.now())
+   print('Dolar: ',cotacao['results']['currencies']['USD']['buy'])
+   print('Euro: ',cotacao['results']['currencies']['EUR']['buy'])
+   print('Bitcoin: ',cotacao['results']['currencies']['BTC']['buy'])
+   time.sleep(2)
+  
